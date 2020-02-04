41ad56788626c0a7cd9d80071f317c0683ef0bd4
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-02-04 10:52:37 +0000

    check map annotation

--- tools/idr/importer.py
+++ tools/idr/importer.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import os
-import sys
 
 from omero.gateway import BlitzGateway
 from omero.constants.namespaces import NSBULKANNOTATIONS
@@ -17,19 +16,40 @@ def find_channel_index(image, channel_name):
             found = True
             break
         count = count + 1
-    # TODO check map annotation for some IDR data
+    # Check map annotation for information. (this is necessary for some images)
+    if not found:
+        for ann in image.listAnnotations(NSBULKANNOTATIONS):
+            pairs = ann.getValue()
+            for p in pairs:
+                if p[0] == "Channels":
+                    channels = p[1].replace(" ", "").split(";")
+                    count = 0
+                    for c in channels:
+                        values = c.split(":")
+                        if channel_name in values:
+                            index = count
+                            found = True
+                            break
+                        count = count + 1
+
     return found, index
 
 
 def get_valid_region(image, x, y, w, h):
     size_x = image.getSizeX()
     size_y = image.getSizeY()
-    if x < 0 or x >= size_x: return None
-    if y < 0 or y >= size_y: return None
-    if w < 0 or w > size_x: return None
-    if h < 0 or h > size_y: return None
-    if x + w > size_x: return None
-    if y + h > size_y: return None
+    if x < 0 or x >= size_x:
+        return None
+    if y < 0 or y >= size_y:
+        return None
+    if w < 0 or w > size_x:
+        return None
+    if h < 0 or h > size_y:
+        return None
+    if x + w > size_x:
+        return None
+    if y + h > size_y:
+        return None
     # TODO improve if region is not set
     return [x, y, w, h]
 
@@ -62,11 +82,10 @@ def parse_ids(ids):
         return ids.replace(" ", "").split(",")
 
 
-
 def main():
 
     ids = parse_ids("1884807")
-    channel_name = "615"
+    channel_name = "PCNT"
     z_section = 0
     timepoint = 0
     region = "0, 0, 10, 20"
@@ -80,8 +99,7 @@ def main():
                         secure=True)
     conn.connect()
 
-    # get the image
-    #image_id = 1884807
+    # Retrieve the images
     for image_id in ids:
         image = conn.getObject("Image", image_id)
         print(image.getName())
