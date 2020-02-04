afbe1cc56279ece9c2df87a9fe491cafc7cb9b31
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-02-04 08:40:04 +0000

    export as tiff

--- tools/idr/importer.py
+++ tools/idr/importer.py
@@ -1,31 +1,51 @@
-import numpy
+import matplotlib.pyplot as plt
 import os
-import tempfile
+import sys
 
 from omero.gateway import BlitzGateway
 
-# connect to idr
-conn = BlitzGateway('public', 'public',
-                    host='ws://idr.openmicroscopy.org/omero-ws',
-                    secure=True)
-conn.connect()
 
-# get the image
-image_id = 1884807
-image = conn.getObject("Image", image_id)
-print(image.getName())
+def main():
+    image_id = 1884807
+    channel_name = "DAPI"
+    z_section = 0
+    timepoint = 0
+    region = "0, 0, 10, 20"
 
-# example loading one single plane and saving it 
-pixels = image.getPrimaryPixels()
-plane = pixels.getPlane(0, 0, 0)  # z, c, t
+    # prepare the area to load
+    tile = region.replace(" ", "").split(",")
 
-# create tmp directory and copy the plane as npy file
-path = tempfile.mkdtemp()
-if not os.path.exists(path):
-    os.makedirs(path)
-filename, file_extension = os.path.splitext(image.getName())
-tmp_file = "plane.npy" # os.path.join(path, filename + ".npy")
-numpy.save(tmp_file, plane)
+    # connect to idr
+    conn = BlitzGateway('public', 'public',
+                        host='ws://idr.openmicroscopy.org/omero-ws',
+                        secure=True)
+    conn.connect()
 
-print(tmp_file)
-conn.close()
+    # get the image
+    #image_id = 1884807
+    image = conn.getObject("Image", image_id)
+    print(image.getName())
+
+    # example loading one single plane and saving it 
+    pixels = image.getPrimaryPixels()
+    # add control
+    x = int(tile[0])
+    y = int(tile[1])
+    w = int(tile[2])
+    h = int(tile[3])
+
+    # Get the channel information
+    # Check if it is the channel metadata if not in the map annotation
+    
+    # Determine channel by name
+    selection = pixels.getTile(theZ=z_section, theT=timepoint, theC=0, tile=[x, y, h, w])
+
+    # save the crop image as TIFF
+    filename, file_extension = os.path.splitext(image.getName())
+    name = filename + "_" + str(image.getId()) + '_'.join(tile) + ".tiff"
+    plt.imsave(name, selection)
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
